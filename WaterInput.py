5f0ec233d296256e810f00d752322d35312dab5d
jjiang03 <112219924+jjiang03@users.noreply.github.com>
2022-11-05 14:51:03 -0700

    Add files via upload

---
+++ WaterInput.py
@@ -0,0 +1,51 @@
+""""
+asks user for the aount of times they performed
+certain event and tracks the amount of times perform
+each task 
+
+stores the count of each task in a text file 
+
+"""
+
+with open('water.txt', 'w+') as f:
+
+    bath_count = 0
+    drink = 0
+    shower = 0
+    wHands = 0
+    dWash = 0
+    laundry = 0
+    garden = 0
+
+    print("Welcome to the Water Tracking Program!")
+    begin_input = int(input("1 for Bathroom\n2 for Drinking Water\n3 for Shower\n4 for Wash Hands\n5 for Dish Washing\n6 Laundry\n7 for Gardening\n8 to quit\n"))
+
+    while begin_input != 8:
+        if begin_input == 1:
+            bath_count += 1
+        elif begin_input == 2:
+            drink += 1
+        elif  begin_input == 3:
+            shower += 1
+        elif begin_input == 4:
+            wHands += 1
+        elif begin_input == 5:
+            dWash += 1
+        elif begin_input == 6:
+            laundry += 1
+        elif begin_input == 7:
+            garden += 1
+        begin_input = int(input("1 for Bathroom\n2 for Drinking Water\n3 for Shower\n4 for Wash Hands\n5 for Dish Washing\n6 Laundry\n7 for Gardening\n8 to quit\n"))
+        
+
+
+    f.write(f'bathroom {bath_count}\n')
+    f.write(f'drink {drink}\n')
+    f.write(f'shower {shower}\n')
+    f.write(f'sink {wHands}\n')
+    f.write(f'dishes {dWash}\n')
+    f.write(f'laundry {laundry}\n')
+    f.write(f'gardening {garden}\n')
+    
+
+    f.close()
