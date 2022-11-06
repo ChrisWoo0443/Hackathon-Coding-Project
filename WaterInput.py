a561da22ed7091abd571527afc01119f893a5f50
jjiang03 <112219924+jjiang03@users.noreply.github.com>
2022-11-05 18:53:58 -0700

    Add files via upload

--- WaterInput.py
+++ WaterInput.py
@@ -23,19 +23,27 @@ with open('water.txt', 'w+') as f:
     while begin_input != 8:
         if begin_input == 1:
             bath_count += 1
+            print("You used the restroom")
         elif begin_input == 2:
             drink += 1
+            print("You drank one bottle of water")
         elif  begin_input == 3:
             shower += 1
+            print("You showered")
         elif begin_input == 4:
             wHands += 1
+            print("You washed your hands")
         elif begin_input == 5:
             dWash += 1
+            print("You washed the dishes")
         elif begin_input == 6:
             laundry += 1
+            print("You did laundry")
         elif begin_input == 7:
             garden += 1
-        begin_input = int(input("1 for Bathroom\n2 for Drinking Water\n3 for Shower\n4 for Wash Hands\n5 for Dish Washing\n6 Laundry\n7 for Gardening\n8 to quit\n"))
+            print("You gardened")
+        begin_input = int(input())
+        
         
 
 
@@ -44,8 +52,8 @@ with open('water.txt', 'w+') as f:
     f.write(f'shower {shower}\n')
     f.write(f'sink {wHands}\n')
     f.write(f'dishes {dWash}\n')
-    f.write(f'laundry {laundry}\n')
     f.write(f'gardening {garden}\n')
+    f.write(f'laundry {laundry}\n')
     
 
     f.close()
